4db560636d562b9e362c74d7d51986d7ce8fbd83
David Gofman <dgofman@gmail.com>
2022-07-18 18:17:41 -0700

    Homework4

--- Homework4/hw4_exercise2.py
+++ Homework4/hw4_exercise2.py
@@ -13,6 +13,9 @@ def largestItem(list, maxItem = -1):
         return largestItem(list, max(maxItem, list.pop(0)))
     return maxItem
 
-if __name__ == "__main__":
+def main():
     maxItem = largestItem([1, 6, 2, 5, 8, 3, 9, 7, 4])
     print("The largest value in the list:", maxItem)
+
+if __name__ == "__main__":
+    main()
